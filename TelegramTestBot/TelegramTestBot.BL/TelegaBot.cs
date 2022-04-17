61667757fd006caa4b76d0487db7b2c5b50b3a5a
Maxim Bespalov <i.maksim_01@mail.ru>
2022-04-17 21:40:44 +0300

    create tg bot

---
+++ TelegramTestBot/TelegramTestBot.BL/TelegaBot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace TelegramTestBot.BL
+{
+    public class TelegaBot
+    {
+        private TelegramBotClient _client;
+        private MainWindow _window;
+
+        public TelegaBot()
+        {
+            _client = new TelegramBotClient("5277457802:AAG5dI1aiAEQYGt08OVjn5snSkX1qbzkc7s");
+
+            _client.StartReceiving(HandleUpdateAsync, );
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type != UpdateType.Message)
+            {
+                return;
+            }
+
+            if (update.Message.Type != MessageType.Text)
+            {
+                return;
+            }
+
+            var chatId = update.Message.Chat.Id;
+            var nickname = update.Message.Chat.Username;
+            var messageText = update.Message.Text;
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -12,12 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TelegramTestBot.BL;
 
 namespace TelegramTestBot.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    private TelegaBot telega;
     public partial class MainWindow : Window
     {
         public MainWindow()
