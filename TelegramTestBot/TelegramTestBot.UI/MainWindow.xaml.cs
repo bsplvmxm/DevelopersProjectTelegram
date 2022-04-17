c1ad85077196bb13fe4323c72870d70883fc052f
Kirill <kirill.vasil.oleg@gmail.com>
2022-04-17 21:47:16 +0300

    Add test creation window

--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TelegramTestBot.UI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
