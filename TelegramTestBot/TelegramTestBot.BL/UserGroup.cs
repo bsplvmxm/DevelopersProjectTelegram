0ca5e58ebf6a08759e5fc15e3032ffc3dc6cb2b0
Maxim Bespalov <i.maksim_01@mail.ru>
2022-04-17 23:47:58 +0300

    add class UserName and UserGroup(not finish)

---
+++ TelegramTestBot/TelegramTestBot.BL/UserGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramTestBot.BL
+{
+    public class UserGroup
+    {
+        public string GroupName { get; private set; }
+
+        public List<UserName> Users { get; private set; }
+
+        public UserGroup()
+        {
+            Users = new List<UserName>();
+            GroupName = "New group";
+        }
+
+        public UserGroup(string groupName)
+        {
+            GroupName = groupName;
+            Users = new List<UserName>();
+        }
+    }
+}
---
+++ TelegramTestBot/TelegramTestBot.BL/UserName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramTestBot.BL
+{
+    public class UserName
+    {
+        public string Name { get; private set; }
+        public long ChatId { get; private set; }
+
+        public UserName(string name, long chatId)
+        {
+            if (name == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            Name = name;
+            ChatId = chatId;
+        }
+
+        public void ChangeName(string realName)
+        {
+            if (realName == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            Name = realName;
+        }
+    }
+}
