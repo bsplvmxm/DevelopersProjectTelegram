fe87b5f5184cbb0a1d10a31ae217407f6720b2d3
Maxim Bespalov <i.maksim_01@mail.ru>
2022-04-18 13:04:06 +0300

    add more edits, need finish

---
+++ TelegramTestBot/TelegramTestBot.BL/TelegaBotManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramTestBot.BL
+{
+    public class TelegaBotManager
+    {
+        private TelegramBotClient _client;
+        private Action<string> _onMessage;
+        //private List<long> _ids;  //для отправки сообщений от бота человеку
+
+
+        public TelegaBotManager(string token, Action<string> onMessage)
+        {
+            _client = new TelegramBotClient(token);
+            _onMessage = onMessage;
+            //_ids = new List<long>();
+        }
+
+        public void StartBot()
+        {
+            _client.StartReceiving(HandleUpdateAsync, HandleErrorAsync);
+        }
+
+        //public void Send(string s)
+        //{
+        //    foreach (var id in _ids)
+        //    {
+        //        InlineKeyboardMarkup inlineKeyboard = new( //кнопочки под сообщением от бота
+        //            new[]
+        //            {
+        //                new []
+        //                {
+        //                    InlineKeyboardButton.WithCallbackData("1","privet"),
+        //                    InlineKeyboardButton.WithCallbackData("2","poka"),
+        //                    InlineKeyboardButton.WithCallbackData("3","textttt"),
+        //                },
+        //                new[]
+        //                {
+        //                    InlineKeyboardButton.WithCallbackData("4","qgqg"),
+        //                    InlineKeyboardButton.WithCallbackData("5","ggggg"),
+        //                },
+        //            });
+
+        //        _client.SendTextMessageAsync(new ChatId(id), s, replyMarkup: inlineKeyboard);
+        //    }
+        //}
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {       
+            if (update.Message != null && update.Message.Text != null)
+            {
+                //if (!_ids.Contains(update.Message.Chat.Id))
+                //{
+                //    _ids.Add(update.Message.Chat.Id);
+                //}
+
+                string s = update.Message.Chat.FirstName+" "+update.Message.Text;
+
+                _onMessage(s);
+            }
+        }
+
+        private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -12,22 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using TelegramTestBot.BL;
 
 namespace TelegramTestBot.UI
 {
     public partial class MainWindow : Window
     {
-        private TelegaBot telegaBot;
+        private TelegaBotManager _telegaManager;
+        private const string _token = "5277457802:AAG5dI1aiAEQYGt08OVjn5snSkX1qbzkc7s";
+        private List<string> _labels;
+        //private DispatcherTimer _timer;  //счетчик времени
 
         public MainWindow()
         {
+            _telegaManager = new TelegaBotManager(_token, OnMessage);
+            _labels = new List<string>();
             InitializeComponent();
+
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = TimeSpan.FromSeconds(1);
+            //_timer.Tick += OnTimerTick;
+            //_timer.Start();
         }
 
+        public void OnMessage(string s)
+        {
+            _labels.Add(s);
+        }
         private void Window_Initialized(object sender, EventArgs e)
         {
-            telegaBot = new TelegaBot(this);
+
         }
+
+        //private void OnTimerTick(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
