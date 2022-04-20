7309efb01362dc376993990231941ddf49565949
baikovIlya <baikusha23@gmail.com>
2022-04-20 22:33:58 +0300

    Add childs class

---
+++ TelegramTestBot/TeleBot.Logic/Questions/AbstractQuestion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleBot.Logic
+{
+    public abstract class AbstractQuestion
+    {
+        public string _question_content;
+
+        public string UserAnswer { get; set; }
+
+        public int _correct_answer;
+
+        public List<string> _answers;
+
+        public AbstractQuestion(string content)
+        {
+            _question_content = content;
+        }
+
+        public void AddAnswer(string answer)
+        {
+            if (answer == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _answers.Add(answer);
+        }
+        public void EditAnswer(int index, string answer)
+        {
+            if (index< 0 || index >= _answers.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _answers[index] = answer;
+        }
+
+        public void DeleteAnswer(int index)
+        {
+            if (_answers.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _answers.RemoveAt(index);
+        }
+
+        public void ChooseCorrect(int index)
+        {
+            _correct_answer = index;
+        }
+
+
+    }
+}
---
+++ TelegramTestBot/TeleBot.Logic/Questions/PollQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleBot.Logic.Questions
+{
+    internal class PollQuestion : AbstractQuestion
+    {
+        public PollQuestion(string content) : base(content)
+        {
+        }
+    }
+}
