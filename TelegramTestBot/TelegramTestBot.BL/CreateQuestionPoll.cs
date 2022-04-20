40a6286bac6c85fd4bbd14d7da3bf2da29fd626d
Maxim Kuzmenko <mway2914@yandex.ru>
2022-04-20 21:08:29 +0300

    add create question for poll

--- TelegramTestBot/TelegramTestBot.BL/CreateQuestionPoll.cs
+++ TelegramTestBot/TelegramTestBot.BL/CreateQuestionPoll.cs
@@ -9,5 +9,33 @@ namespace TelegramTestBot.BL
     class CreateQuestionPoll
     {
         public string NamePoll { get; set; }
+        public List<AbstractQuestion> AbstractQuestions { get; set; }
+
+        public CreateQuestionPoll (string namePoll)
+        {
+            NamePoll = namePoll;
+            AbstractQuestions = new List<AbstractQuestion>();
+        }
+        public void AddQuestion(AbstractQuestion abstractQuestion)
+        {
+            if (abstractQuestion == null)
+            {
+                throw new NullReferenceException();
+            }
+            AbstractQuestions.Add(abstractQuestion);
+        }
+        public void DeleteQuestionPoll(int index)
+        {
+            if (AbstractQuestions.Count < 1)
+            {
+                throw new Exception("List is Empty");
+            }
+            AbstractQuestions.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return NamePoll;
+        }
     }
 }
