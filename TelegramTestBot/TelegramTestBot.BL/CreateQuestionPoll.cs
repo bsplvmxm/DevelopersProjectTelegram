cfa003f5d3ce59b5a457c534734a8eecdd121fd0
Maxim Kuzmenko <mway2914@yandex.ru>
2022-04-20 20:38:38 +0300

    add NamePoll

---
+++ TelegramTestBot/TelegramTestBot.BL/CreateQuestionPoll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramTestBot.BL
+{
+    class CreateQuestionPoll
+    {
+        public string NamePoll { get; set; }
+    }
+}
