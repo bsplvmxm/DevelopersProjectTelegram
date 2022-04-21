1a5d236c6ac23dca9a1014dfe4120dc7e3da7066
Maxim Kuzmenko <mway2914@yandex.ru>
2022-04-21 21:30:15 +0300

    add class Group

---
+++ TelegramTestBot/TelegramTestBot.BL/Group.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramTestBot.BL
+{
+    class Group
+    {
+        public string GroupName { get; set; }
+
+        public List<User> Users { get; set; }
+        public Group(string groupName)
+        {
+            GroupName = groupName;
+            Users = new List<User>();
+        }
+
+        public void AddUser(User user)
+        {
+            if (user == null)
+            {
+                throw new NullReferenceException();
+            }
+            Users.Add(user);
+        }
+
+        public void RemoveUser(User user)
+        {
+            if (user == null)
+            {
+                throw new NullReferenceException();
+            }
+            Users.Remove(user);
+        }
+
+        public override string ToString()
+        {
+            return GroupName;
+        }
+    }
+}
