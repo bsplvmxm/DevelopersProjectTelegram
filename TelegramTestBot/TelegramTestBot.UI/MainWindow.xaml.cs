f7b7b2a6e415946c768147c9aa28566d76a39089
Maxim Kuzmenko <mway2914@yandex.ru>
2022-04-25 10:30:38 +0300

    add visible

--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -101,6 +101,7 @@ namespace TelegramTestBot.UI
 
         private void LB_AllTests_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            HideAllForTest();
             string nameOfTest = (string)LB_AllTests.SelectedItem;
             TextBl_NameTest.Text = nameOfTest;
             TB_NameOfTest.Text = nameOfTest;
@@ -119,29 +120,30 @@ namespace TelegramTestBot.UI
             //LB_QuestOfTest.Items.Add(newQuest);
         }
 
-        //private void HideAllForTest()
-        //{
-        //    Label_ViewQuest.Visibility = Visibility.Hidden;
-        //    CB_TypeQuestion.Visibility = Visibility.Hidden;
-        //    RB_RightAns1.Visibility = Visibility.Hidden;
-        //    RB_RightAns2.Visibility = Visibility.Hidden;
-        //    RB_RightAns3.Visibility = Visibility.Hidden;
-        //    RB_RightAns4.Visibility = Visibility.Hidden;
-        //    TB_Answer1.Visibility = Visibility.Hidden;
-        //    TB_Answer2.Visibility = Visibility.Hidden;
-        //    TB_Answer3.Visibility = Visibility.Hidden;
-        //    TB_Answer4.IsEnabled = false;
-        //    ChB_RightAns1.IsEnabled = false;
-        //    ChB_RightAns2.IsEnabled = false;
-        //    ChB_RightAns3.IsEnabled = false;
-        //    ChB_RightAns4.IsEnabled = false;
-        //    Label_InputQuest.IsEnabled = false;
-        //    TB_QuestionContent.IsEnabled = false;
-        //    Button_CreateQuest.IsEnabled = false;
-        //    Button_EditQuest.IsEnabled = false;
-        //    Button_DeleteQuest.IsEnabled = false;
-        //    Button_RenameTest.IsEnabled = false;
-        //}
+        private void HideAllForTest()
+        {
+            Label_ViewQuest.Visibility = Visibility.Visible;
+            CB_TypeQuestion.Visibility = Visibility.Visible;
+            RB_RightAns1.Visibility = Visibility.Visible;
+            RB_RightAns2.Visibility = Visibility.Visible;
+            RB_RightAns3.Visibility = Visibility.Visible;
+            RB_RightAns4.Visibility = Visibility.Visible;
+            TB_Answer1.Visibility = Visibility.Visible;
+            TB_Answer2.Visibility = Visibility.Visible;
+            TB_Answer3.Visibility = Visibility.Visible;
+            TB_Answer4.Visibility = Visibility.Visible;
+            ChB_RightAns1.Visibility = Visibility.Visible;
+            ChB_RightAns2.Visibility = Visibility.Visible;
+            ChB_RightAns3.Visibility = Visibility.Visible;
+            ChB_RightAns4.Visibility = Visibility.Visible;
+            Label_InputQuest.Visibility = Visibility.Visible;
+            TB_QuestionContent.Visibility = Visibility.Visible;
+            Button_CreateQuest.Visibility = Visibility.Visible;
+            Button_EditQuest.Visibility = Visibility.Visible;
+            Button_DeleteQuest.Visibility = Visibility.Visible;
+            Button_RenameTest.Visibility = Visibility.Visible;
+            LB_QuestOfTest.Visibility = Visibility.Visible;
+        }
 
 
     }
