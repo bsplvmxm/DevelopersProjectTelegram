15de9f9b964f51c5ae1d1177809ca79bd406b505
baikovIlya <baikusha23@gmail.com>
2022-05-04 15:32:37 +0300

    Add excel report, did not tested

--- TelegramTestBot/TelegramTestBot.BL/Data/TestsBase.cs
+++ TelegramTestBot/TelegramTestBot.BL/Data/TestsBase.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using TelegramTestBot.BL;
 using TelegramTestBot.BL.Questions;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace TelegramTestBot.BL.Data
 {
@@ -60,5 +62,71 @@ namespace TelegramTestBot.BL.Data
             }
         }
 
+        public void CreateTestReport(string nameOfGroup, Test currentTest)
+        {
+            string reportName = $"Отчёт по группе {nameOfGroup}, тест: {currentTest.NameTest}";
+            //лист с именами пользователей
+            List<string> usersNames = new List<string> { };
+            if (BaseOfUsers.GroupBase.ContainsKey(nameOfGroup))
+            {
+                foreach (var users in BaseOfUsers.NameBase)
+                {
+                    if (BaseOfUsers.GroupBase[nameOfGroup].Contains(users.Value))
+                    {
+                        usersNames.Add(users.Value);
+                    }
+                }
+            }
+            //инициальзируем эксель
+            Excel.Application oXL;//приложение
+            Excel._Workbook report;//сам файл
+            Excel._Worksheet oSheet;//лист
+            Excel.Range oRng;//хз но надо
+            oXL = new Excel.Application();
+            oXL.Visible = true;
+            oXL.SheetsInNewWorkbook = 2;//кол-во листов в книге
+            report = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+            oSheet = (Excel._Worksheet)report.Worksheets[1];//выцепляем нужный лист
+            oSheet.Cells[1, 1] = $"{reportName}";//А1 = название отчёта
+            oSheet.Name = $"{reportName}";//имя листа(на вкладке снизу) = название отчёта
+            //заполняем первую строку именами юзеров, начаиния с В1 идём С1, D1 и т.д.
+            for(int i = 0; i <= usersNames.Count; i++)
+            {
+                oSheet.Cells[i+2, 1] = $"{usersNames[i]}";
+            }
+            //заполняем первый столбец вопросами теста, начиная с А2 идём А3, А4 и т.д.
+            for(int i = 0; i < currentTest.Questions.Count; i++)
+            {
+                //создаём стрингу с вопросом и вариантами ответов на него
+                string currentQuestion = "";
+                currentQuestion = currentTest.Questions[i].ContentOfQuestion;
+                foreach(string answer in currentTest.Questions[i].Answers)
+                {
+                    currentQuestion += $" {answer}";
+                }
+                //заполняем ячейку
+                oSheet.Cells[1,i+2] = $"{currentQuestion}";
+            }
+            //заполняем ответами начиная с В2 идём В3, В4 и т.д.
+            for (int i = 0; i < usersNames.Count; i++)
+            {
+                string[] an = new string[currentTest.Questions.Count]; //массив ответов, эт переделаем в лист
+                for(int j = 0; j < currentTest.Questions.Count; j++)
+                {
+                    if (an[j] != null)
+                    {
+                        oSheet.Cells[i + 2, j + 2] = $"{an[j]}";//ответ на конкретный вопрос
+                    }
+                    else
+                    {
+                        oSheet.Cells[i + 2, j + 2] = "Нет ответа";//если не успел
+                    }
+                }
+            }
+            // сохранение с именем которое в начале задавали
+            report.Application.ActiveWorkbook.SaveAs($"{reportName}.xls", Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+        }
     }
 }
